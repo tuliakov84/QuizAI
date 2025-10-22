5470ca2d41ad9130cdb14a39b97c74485bf02387
Туляков Евгений Юрьевич <zz21marcel@gmail.com>
2025-10-23 01:02:25 +0300

    Create MainTestExample.java

---
+++ ‎src/test/java/com.mipt/MainTestExample.java
@@ -0,0 +1,12 @@
+package com.mipt;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTestExample {
+
+  @Test
+  void testClass() {
+    assertTrue(true);
+  }
+}
