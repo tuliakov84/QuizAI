0ce844d073378a24ff4ae8e4935388fa4cbcbda1
str4teg <say2005@yandex.ru>
2025-11-05 01:41:32 +0300

    fix2

--- src/main/java/com/mipt/domainModel/Achievement.java
+++ src/main/java/com/mipt/domainModel/Achievement.java
@@ -12,7 +12,7 @@ public class Achievement implements IAchievement {
   public int globalRatingPlaceNeeded;
   public int currentGamePointsNeeded;
   public int currentGameRatingNeeded;
-  public int currentGameDifficultyNeeded;
+  public int currentGameLevelDifficultyNeeded;
 
   public Achievement(int achievementId, String name) {
     this.achievementId = achievementId;
@@ -110,12 +110,12 @@ public class Achievement implements IAchievement {
   }
 
   @Override
-  public int getCurrentGameDifficultyNeeded() {
-    return currentGameDifficultyNeeded;
+  public int getCurrentGameLevelDifficultyNeeded() {
+    return currentGameLevelDifficultyNeeded;
   }
 
   @Override
-  public void setCurrentGameDifficultyNeeded(int currentGameDifficultyNeeded) {
-    this.currentGameDifficultyNeeded = currentGameDifficultyNeeded;
+  public void setCurrentGameLevelDifficultyNeeded(int currentGameLevelDifficultyNeeded) {
+    this.currentGameLevelDifficultyNeeded = currentGameLevelDifficultyNeeded;
   }
 }
