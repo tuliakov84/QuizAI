1e9e1fa6b02c62fba278c2fa380e9f883781feb9
str4teg <say2005@yandex.ru>
2025-11-05 03:16:36 +0300

    Added apiController interface

---
+++ src/main/java/com/mipt/api/IApiController.java
@@ -0,0 +1,34 @@
+package com.mipt.api;
+
+import com.mipt.domainModel.*;
+
+import java.util.List;
+
+public interface IApiController {
+
+  User register(String username, String password);
+  String login(String username, String password);
+  void logout(String session);
+
+  User getUserBySession(String session);
+  void updateProfilePic(String session, int picId);
+  void updateDescription(String session, String description);
+
+  Game createGame(String session, int topicId, Game.LevelDifficulty level, boolean isPrivate);
+  List<Game> listWaitingGames();
+  void joinGame(String session, int gameId);
+  void startGame(String session, int gameId);
+  void pauseGame(String session, int gameId);
+  void endGame(String session, int gameId);
+
+  Question getNextQuestion(String session, int gameId);
+  boolean answerQuestion(String session, int gameId, int answerNumber);
+
+  List<Topic> listTopics();
+
+  List<Achievement> getAllAchievements();
+  List<Achievement> getUserAchievements(String session);
+
+  List<User> getGlobalRating();
+  List<User> getCurrentGameRating(int gameId);
+}
