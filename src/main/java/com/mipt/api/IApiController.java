c10ac3fbc01b377e5e67a977493dc496961d41b4
str4teg <say2005@yandex.ru>
2025-11-21 01:45:43 +0300

    Обновлён IApiController (не закончен)

--- src/main/java/com/mipt/api/IApiController.java
+++ src/main/java/com/mipt/api/IApiController.java
@@ -7,48 +7,21 @@ import java.time.Instant;
 import java.util.List;
 
 public interface IApiController {
-  //User authentification
-  String register(String userName, String password);
-  String login(String userName, String password) throws AccessException;
-  void logOut(String session);
+  User login(String username, String password);
+  User register(String username, String password);
+  void logout(String session);
 
-  //Show UserData
-  String getUsername(String session);
-  int getProfilePicId(String session);
-  int getCurrentGlobalPoints(String session);
-  int getGlobalPossiblePoints(String session);
-  String getDescription(String session);
-  List<Achievement> getAchievements(String session);
-  List<Game> getGamesPlayed(String session);
+  User getUser(String session);
+  User updateUser(String session, int newPicId, String newDescription, String newUsername);
 
+  List<Game> getLast5GamesPlayed(String session);
+  List<Game> getAllGamesPlayed(String session);
 
-  //Editing profile
-  User changeUserProfilePic(String session, int picId);
-  User changeUsername(String session, String newUserName);
-  User changePassword(String session, String oldPassword, String newPassword);
-  User changeDescription(String session, String newDescription);
-
-  //Host operations
-  Game createGame(String hostSession, int topicId, Game.LevelDifficulty level,
-                  boolean isPrivate, int maxAmountOfPlayers);
-  Game changeMaxAmountOfPlayersInGame(String hostSession, int newMaxAmountOfPlayers);
-  Question startGame(String hostSession);
-  void deleteGame(String hostSession);
-  void pauseGame(String hostSession);
-  void resumeGame(String hostSession);
-
-  //Nonhost operations
+  void joinGame(String session, int gameId);
+  List<Topic> getAllTopics();
   List<Game> getListOfOpenGames();
-  User joinGame(String session, int gameId);
-  User leftGame(String session);
-  List<User> getListOfPlayersInCurrentGame(int gameId);
 
-  //Game service
-  Question getNextQuestion(int questionId, int gameId);
-  void getAnswer(String session, int answerNumber, Instant wastedTime);
-  int getCurrentGamePoints(String session);
-  void endGame(int gameId);//should it be in ApiController?
-
-  //Show leaderboard
-  List<User> getGlobalLeaderboard();
+  Game createGame(int difficulty, boolean isPrivate, int participantsNumber,
+                  int numberOfQuestions, String topic);
+  void joinGame();
 }
