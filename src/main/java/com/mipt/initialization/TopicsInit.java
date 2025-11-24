94d69f5c038d7cbff9f63bb3ecd84432f568cd07
vladislav <vladislav.fokin81@gmail.com>
2025-11-24 20:59:48 +0300

    new features in ApiController, DbService

--- src/main/java/com/mipt/initialization/TopicsInit.java
+++ src/main/java/com/mipt/initialization/TopicsInit.java
@@ -2,7 +2,12 @@ package com.mipt.initialization;
 
 import com.mipt.dbAPI.DbService;
 import com.mipt.domainModel.Topic;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class TopicsInit {
@@ -12,13 +17,24 @@ public class TopicsInit {
     this.dbService = dbService;
   }
 
-  public void topicsInit() {
-    Topic topic = new Topic();
-    topic.setName("My Topic");
-    try {
-      dbService.addTopic(topic);
-    } catch (SQLException e) {
-      return;
+  public void topicsInit() throws SQLException {
+    Topic[] dbTopicList = dbService.getAllTopics();
+    if (dbTopicList.length == 0) {
+      JSONParser parser = new JSONParser();
+
+      try (FileReader reader = new FileReader("topics.json")) {
+        Object obj = parser.parse(reader);
+        JSONObject jsonObject = (JSONObject) obj;
+        
+        String name = (String) jsonObject.get("name");
+        long age = (long) jsonObject.get("age");
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+      } catch (IOException | ParseException e) {
+        System.out.println(e.getMessage());
+      }
     }
   }
 }
