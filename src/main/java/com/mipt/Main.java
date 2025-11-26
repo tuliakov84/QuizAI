5378b5e724b4a977c38a6c9fc4ae30badcb01cef
Liber Anzhelika <liber.an@phystech.edu>
2025-11-26 17:29:00 +0300

    Update Main.java

--- src/main/java/com/mipt/Main.java
+++ src/main/java/com/mipt/Main.java
@@ -1,7 +1,33 @@
 package com.mipt;
 
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
-  public static void main(String[] args) {
-    
-  }
+
+    public static void main(String[] args) throws Exception {
+
+        String jsonString = "[{\"topic\":\"Ocean\",\"n\":10,\"difficult\":2}]";
+
+        CompletableFuture<String> futureJson =
+                QuestionGenerator.generate(jsonString);
+
+        calculator();
+
+        String resultJson = futureJson.join();
+
+        System.out.println(resultJson);
+    }
+
+    public static void  calculator() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println(a + b);
+        }
+    }
 }
